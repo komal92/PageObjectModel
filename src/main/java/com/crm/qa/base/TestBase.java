8e3fb26afa6d2a7a3f61ae4fe63cd2477abd713b
Komal Hukmatani <khukmatani@xpanxion.com>
2019-12-31 14:10:29 +0530

    Correcting asserts, and adding driver files

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,11 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "E:/Jenkins POC/Drivers/chromedriver_win32/chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+			System.setProperty("webdriver.gecko.driver", "C:/Firefox60.5.2ESR/firefox.exe");	
 			driver = new FirefoxDriver(); 
 		}
 		
